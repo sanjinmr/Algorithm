c430d08dd8ac79605298a0daa2626c02d7147bfa
dongmingxin <dongmingxin@yinyueke.com>
2019-03-15 09:17:42 +0800

    first commit

---
+++ app/src/main/java/com/sanjin/algorithm/AlgorithmShell.java
@@ -0,0 +1,41 @@
+package com.sanjin.algorithm;
+
+/**
+ * author: sanji
+ * email: sanjinmr@sina.com
+ * time: 2019/3/14
+ * desc:
+ * note:
+ */
+public class AlgorithmShell {
+
+    public static void groupSort(int[] a, int n, int i, int gap) {
+        System.out.println(" ,gap: " + gap);
+        for (int j = i + gap; j < n; j += gap) {
+            System.out.print(" ,j: " + j);
+            if (a[j] < a[j - gap]) {
+                int temp = a[j];
+                int k = j - gap;
+                System.out.print(" ,k: " + k);
+                while (k >= 0 && a[k] > temp) {
+                    a[k +gap] = a[k];
+                    k -= gap;
+                    System.out.println(" ,k1: " + k);
+                }
+                a[k + gap] = temp;
+            }
+        }
+    }
+
+    public static void shellSort2(int[] a, int n) {
+        int sum = 0;
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            System.out.println("\n<=========>");
+            System.out.println("第" + sum++ + "次增量： " + gap);
+            for (int i = 0; i < gap; i ++) {
+                System.out.println("第" + i + "次排序，增量： " + gap);
+                groupSort(a, n, i, gap);
+            }
+        }
+    }
+}
---
+++ app/src/test/java/com/sanjin/algorithm/ExampleUnitTest.java
@@ -0,0 +1,31 @@
+package com.sanjin.algorithm;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void shellAlgorithmTest() {
+        int[] a = {80, 30, 60, 40, 20, 10, 50, 70};
+
+        AlgorithmShell.shellSort2(a, a.length);
+
+        System.out.println("");
+        System.out.println("排序后：");
+        for (int i = 0; i < a.length; i ++) {
+            System.out.print(a[i] + "; ");
+        }
+        System.out.println("");
+    }
+}
