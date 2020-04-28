54ef8be1f4bafe95732d2f7ef1cdaa3e1dc61767
sanjinmr <sanjinmr@sin.com>
2020-04-28 19:02:53 +0800

    修改希尔排序

--- app/src/main/java/com/sanjin/www/algorithm/insertsort/shellsort/AlgorithmShell.java
+++ app/src/main/java/com/sanjin/www/algorithm/insertsort/shellsort/AlgorithmShell.java
@@ -4,8 +4,9 @@ package com.sanjin.www.algorithm.insertsort.shellsort;
  * author: sanji
  * email: sanjinmr@sina.com
  * time: 2019/3/14
- * desc: 无log
- * note: 希尔排序
+ * desc: 希尔排序
+ * note: 希尔排序实际上就是对直接插入排序的一个升级：
+ * 将数组分为若干组进行直接插入排序，只是将插入排序的步长改为而已！
  */
 public class AlgorithmShell {
 
@@ -55,6 +56,76 @@ public class AlgorithmShell {
         for (int gap = n / 2; gap > 0; gap /= 2) {
             for (int i = 0; i < gap; i ++) {
                 directInsertSort(a, n, i, gap);
+            }
+        }
+    }
+
+    /**
+     * 用最原始的直接插入排序写一个希尔排序
+     * @param a
+     * @param n
+     */
+    public static void shellSort1(int[] a, int n) {
+        int j, k;
+        for (int gap = n / 2; gap > 0; gap = gap / 2) { // shell分组
+            for (int gi = 0; gi < gap; gi ++) { // 遍历每一组
+                for (int i = gi + gap; i < n; i = i + gap) { // 对每一组进行直接插入排序
+                    for (j = i - gap; j > 0; j = j - gap) {
+                        if (a[j] <= a[i]) {
+                            break;
+                        }
+                    }
+
+                    if (j != i - gap) {
+                        int temp = a[i];
+                        for (k = i - gap; k > j; k = k - gap) {
+                            a[k + gap] = a[k];
+                        }
+                        a[k + gap] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void shellSort2(int[] a, int n) {
+        int k;
+        for (int gap = n / 2; gap > 0; gap = gap / 2) { // shell分组
+            for (int gi = 0; gi < gap; gi ++) { // 遍历每一组
+                for (int i = gi + gap; i < n; i = i + gap) { // 对每一组进行直接插入排序
+                    if (a[i - gap] > a[i]) {
+                        int temp = a[i];
+                        for (k = i - gap; k >= 0 && a[k] > temp; k = k - gap) {
+                            a[k + gap] = a[k];
+                        }
+                        a[k + gap] = a[k];
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 在这里放一个最原始的步长为1的直接插入排序以作参考
+     * @param a
+     * @param n
+     */
+    public static void insertShort(int [] a,  int n) {
+        int i, j, k;
+
+        for (i= 1; i < n; i++) {
+            for (j = i - 1; j >= 0; j --) {
+                if (a[j] <= a[i]) {
+                    break;
+                }
+            }
+
+            if (j != i -1) {
+                int temp = a[i];
+                for (k = i -1; k > j; k --) {
+                    a[k + 1] = a[k];
+                }
+                a[k + 1]  = temp;
             }
         }
     }
